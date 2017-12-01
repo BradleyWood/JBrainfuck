573b73d4ea572710b673354fcad53fecfe21d434
BradleyWood <bradley.wood@uoit.net>
2017-12-01 13:43:12 -0500

    added main class

--- src/main/java/org/jbrainfuck/Application.java
+++ src/main/java/org/jbrainfuck/Application.java
@@ -1,8 +1,27 @@
 package org.jbrainfuck;
 
+import org.jbrainfuck.antlr.BFParser;
+import org.jbrainfuck.interpreter.Interpreter;
+import org.jbrainfuck.program.Program;
+
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Must specify source file!");
+            return;
+        }
 
+        String file = args[0];
+
+        BFParser parser = new BFParser(file);
+        Program program = parser.parse();
+        System.out.println("-------Parsed Input-------");
+        System.out.println(program);
+        System.out.println("--------------------------");
+        System.out.println();
+
+        Interpreter interpreter = new Interpreter(program);
+        interpreter.run();
     }
 }
